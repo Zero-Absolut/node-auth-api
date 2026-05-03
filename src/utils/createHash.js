db56e24c5f408ff81b8b0897861d2efad2c4ecf8
Mateus Brito <lestarfelipe@gmail.com>
2026-05-03 16:55:01 -0300

    Atualizar o createHash.js

--- src/utils/createHash.js
+++ src/utils/createHash.js
@@ -4,9 +4,9 @@ export const createHash = async (value) => {
   try {
     if (!value) return null;
 
-    const hash = await bcrypt.hash(value, 10);
+    return await bcrypt.hash(value, 10);
 
-    return hash;
+    
   } catch (err) {
     console.error("Erro no processamento", err);
 
